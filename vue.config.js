38b4a37401a8eb07943862315fb6375cc1cdc4e2
Hemant Kumar <hemant.kumar671@gmail.com>
2023-03-31 12:15:09 +0530

    updated

--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,7 @@ module.exports = defineConfig({
   devServer: {
     proxy: {
       '^/api':{
-        target: 'http://localhost:3000',
+        target: 'https://jovial-macaron-daff9d.netlify.app:3000',
         changeOrigin: true,
         logLevel: 'debug',
         pathRewrite: {'^/api':'/'}
